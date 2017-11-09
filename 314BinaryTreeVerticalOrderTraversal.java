a6a81e0095e710c2e79865af98b30bb60c199ce6
Jie Wu <wjbua@wuji.redmond.corp.microsoft.com>
2017-11-08 23:16:44 -0800

    add 314

---
+++ 314BinaryTreeVerticalOrderTraversal.java
@@ -0,0 +1,124 @@
+import java.util.ArrayList;
+
+/*
+ * 
+ * 314.	Binary Tree Vertical Order Traversal
+Given a binary tree, return the vertical order traversal of its nodes' values. (ie, from top to bottom, column by column).
+
+If two nodes are in the same row and column, the order should be from left to right.
+
+Examples:
+Given binary tree [3,9,20,null,null,15,7],
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+ 
+
+return its vertical order traversal as:
+
+[
+  [9],
+  [3,15],
+  [20],
+  [7]
+]
+ 
+
+Given binary tree [3,9,20,4,5,2,7],
+
+    _3_
+   /   \
+  9    20
+ / \   / \
+4   5 2   7
+ 
+
+return its vertical order traversal as:
+
+[
+  [4],
+  [9],
+  [3,5,2],
+  [20],
+  [7]
+]
+
+ */
+
+public class Solution {
+
+	 public static List<List<Integer>> verticalOrder(TreeNode root) 
+	    {
+	    	List<List<Integer>> ret = new ArrayList<>();
+	    	TreeMap<Integer, List<Integer>> map = new TreeMap<>();
+	    	verticalorderHelper(root, map, 0);
+	    	for(Entry<Integer, List<Integer>> entry : map.entrySet())
+	    	{
+	    		ret.add(entry.getValue());
+	    		System.out.println(entry.getValue());
+	    	}
+	    	
+	    	return ret;
+	    }
+	    
+	    private static void	verticalorderHelper(TreeNode node, TreeMap<Integer, List<Integer>> map, int depth)
+	    {
+	    	if(node == null)
+	    	{
+	    		return;
+	    	}
+	    	
+	    	if(!map.containsKey(depth))
+	    	{
+	    		map.put(depth, new ArrayList<Integer>());
+	    	}
+	    	
+	    	map.get(depth).add(node.val);
+	    	verticalorderHelper(node.left, map, depth-1);
+	    	verticalorderHelper(node.right, map, depth+1);
+	    }
+	    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}

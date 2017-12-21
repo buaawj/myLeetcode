249f64256c5bd0748a6a164b1082d17bc730135a
Jie Wu <wjbua@wuji.redmond.corp.microsoft.com>
2017-12-20 23:16:21 -0800

    Update 353

--- 353DesignSnakeGame.java
+++ 353DesignSnakeGame.java
@@ -51,68 +51,68 @@ snake.move("L"); -> Returns 2 (Snake eats the second food)
 snake.move("U"); -> Returns -1 (Game over because snake collides with border)
  */
 
-public class SnakeGame {
+ public static class SnakeGame {
 
-	int width;
-	int height;
-	int row;
-	int col;
-	int credit;
-	Queue<Integer> pos;
-	Map<String, int[]> mapping;
-	
-	/** Initialize your data structure here.
-    	@param width - screen width
-    	@param height - screen height 
-    	@param food - A list of food positions
-    E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */	   
-	public SnakeGame(int width, int height, int[][] food) 
-	{
-		this.width = width;
-		this.height = height;
-		this.row = 0;
-		this.col = 0;
-		this.credit = 0;
-		this.queue = new LinkedList<>();
-		for(int[] item : food)
-		{
-			Queue.offer(item[0]*row + item[1]);
-		}
-		
-		mapping = new HashMap<>();
-		mapping.add("U", {-1, 0});
-		mapping.add("L", {0, -1});
-		mapping.add("R", {0, 1});
-		mapping.add("D", {1, 0});
-	}
-	   
-	/** Moves the snake.
-       @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down 
-       @return The game's score after the move. Return -1 if game over. 
-       Game over when snake crosses the screen boundary or bites its body. */
-   public int move(String direction) 
-   {
-	   int newrow = this.row + mapping.get(direction)[0];
-	   int newcol = this.col + mapping.get(direction)[1];
-	   if(newrow>=this.width || newrow<0 || newcol >=this.height || newcol<0)
-		   return -1;
-	   this.row = newrow;
-	   this.col = newcol;
-	   
-	   if(!Queue.isEmpty())
-	   {
-		   int foodrow = queue.peek()/this.width;
-		   int foodcol = queue.peek()%this.width;
-		   if(foodrow == newrow && foodcol == newcol)
-		   {
-			   this.credit++;
-			   Queue.poll();
-		   }
-	   }
-	   
-	   return this.credit;
-   }
+             int width;
+             int height;
+             int credit;
+             Deque<int[]> visited;
+             int[][] food;
+             Map<String, int[]> mapping;
 
+             /** Initialize your data structure here.
+                 @param width - screen width
+                 @param height - screen height
+                 @param food - A list of food positions
+             E.g food = [[1,1], [1,0]] means the first food is positioned at [1,1], the second is at [1,0]. */
+             public SnakeGame(int width, int height, int[][] food)
+             {
+                 this.width = width;
+                 this.height = height;
+                 this.credit = 0;
+                 this.visited = new LinkedList<>();
+                 this.visited.add(new int[]{0, 0});
+                 this.food = food;
+                 mapping = new HashMap<String, int[]>();
+                 mapping.put("U", new int[]{-1, 0});
+                 mapping.put("L", new int[]{0, -1});
+                 mapping.put("R", new int[]{0, 1});
+                 mapping.put("D", new int[]{1, 0});
+             }
+
+             /** Moves the snake.
+                @param direction - 'U' = Up, 'L' = Left, 'R' = Right, 'D' = Down
+                @return The game's score after the move. Return -1 if game over.
+                Game over when snake crosses the screen boundary or bites its body. */
+            public int move(String direction)
+            {
+                int[] head = visited.getFirst(), tail = visited.getLast();
+                visited.removeLast();
+                int newrow = head[0] + mapping.get(direction)[0];
+                int newcol = head[1] + mapping.get(direction)[1];
+                if(newrow>=this.width || newrow<0 || newcol >=this.height || newcol<0)
+                    return -1;
+                for(int[] pos : visited)
+                {
+                    if(pos[0] == newrow && pos[1] == newcol)
+                        return -1;
+                }
+
+                if(this.credit < food.length)
+                {
+                    if(food[this.credit][0] == newrow && food[this.credit][1] == newcol)
+                    {
+                        this.credit++;
+                        visited.addLast(tail);
+                        visited.addFirst(new int[] {newrow, newcol});
+                    }
+                }
+
+                return this.credit;
+            }
+
+
+         }
 }
 
 

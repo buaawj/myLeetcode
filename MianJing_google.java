28c74c65b9d7fa981a79d41117f85670c4a4a53f
Jie Wu <wuji@microsoft.com>
2019-05-28 15:32:44 -0700

    udpate google mianjing

--- MianJing_google.java
+++ MianJing_google.java
@@ -65,7 +65,120 @@ import java.util.ArrayList;
      return ret;
  }
 
+/* Decode string: "{a,b}c{d,e}f" --> [acdf, acef, bcdf, bcef] BFS*/
 
+	static Queue<String> decodeString(String s)
+	{
+		List<String> ret = new ArrayList<>();
+		Queue<String> q = new LinkedList<>();
+		int i = 0, n = s.length();
+
+		while(i<n)
+		{
+			int j=i;
+			while(j<n && s.charAt(j) !='{') ++j;
+			update(q, new String[] {s.substring(i, j)});
+			int start = j+1;
+			if(start >= n) break;
+			while(j<n && s.charAt(j) !='}') ++j;
+			update(q, s.substring(start, j).split(","));
+			i = j+1;
+		}
+
+		return q;
+	}
+
+	static void update(Queue<String> q, String[] strs)
+	{
+		if(q.isEmpty()) q.add("");
+		for(int i=q.size()-1; i>=0; --i)
+		{
+			String cur = q.poll();
+			for(String s : strs)
+				q.offer(cur+s);
+		}
+	}
+
+/*fOLLOWUP: "a{b{c, d}e{f}}""->  [abcef, abdef]*/
+static void dfs(String s, List<String>ret)
+	{
+		if(!s.contains("{"))
+		{
+			ret.add(s);
+			return;
+		}
+		Queue<String> ret1 = decodeString(s);
+		for(String s1 : ret1)
+			dfs(s1, ret);
+	}
+
+	static Queue<String> decodeString(String s)
+	{
+		Queue<String> q = new LinkedList<>();
+		int i = 0, n = s.length();
+
+		while(i<n)
+		{
+			int j=i;
+			while(j<n && s.charAt(j) !='{') ++j;
+			update(q, new String[] {s.substring(i, j)});
+			int start = j+1;
+			if(start >= n) break;
+			int cnt = 1;
+			++j;
+			while(j<n && cnt > 0)
+			{
+				if(s.charAt(j) =='{') ++cnt;
+				else if(s.charAt(j) =='}') --cnt;
+				++j;
+			}
+			--j;
+			String substr = s.substring(start, j);
+			update(q, splitString(substr));
+			i = j+1;
+		}
+
+		return q;
+	}
+
+
+	static String[] splitString(String s)
+	{
+		List<String> ret = new ArrayList<>();
+		int cnt = 0, i=0, n= s.length();
+		int start = 0;
+		while(i<n)
+		{
+			if(s.charAt(i) =='{')
+			{
+				++cnt;
+			}
+			else if(s.charAt(i) =='}') --cnt;
+			else if(s.charAt(i) ==',' && cnt ==0)
+			{
+				ret.add(s.substring(start, i));
+				start = i+1;
+			}
+			++i;
+		}
+
+		if(start < n)
+			ret.add(s.substring(start));
+
+		return ret.toArray(new String[ret.size()]);
+	}
+
+
+	static void update(Queue<String> q, String[] strs)
+	{
+		if(q.isEmpty()) q.add("");
+		for(int i=q.size()-1; i>=0; --i)
+		{
+			String cur = q.poll();
+			for(String s : strs)
+				q.offer(cur+s);
+		}
+	}
 
 
 
